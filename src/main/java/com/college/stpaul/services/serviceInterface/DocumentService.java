c0c534a84f2df47c5aa04418e1188a0d54b037b6
mayu-code <durbulemayu@gmail.com>
2025-01-09 12:06:16 +0530

    sync

---
+++ src/main/java/com/college/stpaul/services/serviceInterface/DocumentService.java
@@ -0,0 +1,8 @@
+package com.college.stpaul.services.serviceInterface;
+
+import com.college.stpaul.entities.Documents;
+
+public interface DocumentService {
+    Documents addDocuments(Documents documents);
+    
+}
